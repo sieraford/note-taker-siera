5665e5a6a76117d2b2ee745bf0453425629019fe
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-12-30 15:59:56 -0800

    create server.js file and add express routes

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const dbData = require('./db/db.json');
+const PORT = 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+app.get('/api/notes', (req, res) => res.json(dbData));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
