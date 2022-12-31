492d1ed4b38ac4dd57a34ca5193cc37cf761e0eb
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-12-31 14:53:33 -0800

    fixed post request so that dats gets saved to db.json file

--- Develop/server.js
+++ Develop/server.js
@@ -25,7 +25,7 @@ app.get('/notes', (req, res) => {
 
 app.get('/api/notes', (req, res) => res.json(dbData));
 
-// POST request to add a review
+// POST request to add a note
 app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
@@ -39,20 +39,31 @@ app.post('/api/notes', (req, res) => {
     const newNote = {
       title,
       text,
-      note_id: uuid(),
+      id: uuid(),
     };
 
-    // Convert the data to a string so we can save it
-    const noteString = JSON.stringify(newNote);
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
 
-    // Write the string to a file
-    fs.appendFile(`./db/db.json`, noteString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Note for ${newNote.title} has been written to JSON file`
-          )
-    );
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
 
     const response = {
       status: 'success',
