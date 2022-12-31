e4a7543be79d9b7fc35bfb60c35d438517c3fefd
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2022-12-31 11:55:34 -0800

    add post route

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const path = require('path');
 const dbData = require('./db/db.json');
+const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
 const PORT = 3001;
 
 const app = express();
@@ -20,6 +24,47 @@ app.get('/notes', (req, res) => {
   });
 
 app.get('/api/notes', (req, res) => res.json(dbData));
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    // Convert the data to a string so we can save it
+    const noteString = JSON.stringify(newNote);
+
+    // Write the string to a file
+    fs.appendFile(`./db/db.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Note for ${newNote.title} has been written to JSON file`
+          )
+    );
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
